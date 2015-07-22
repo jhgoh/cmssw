859cba63d9ee7f6388f08b52c24d34a88fe8ebf0
Junghwan John Goh <jhgoh@cern.ch>
2015-07-23 01:53:35 +0200

    correction function by James

--- GeneratorInterface/RivetInterface/src/CMS_AN_PseudoTop.cc
+++ GeneratorInterface/RivetInterface/src/CMS_AN_PseudoTop.cc
@@ -39,6 +39,7 @@ public:
     _h29_diffXSecTopDiLepPartonttbarPt         = bookHisto1D("h29_diffXSecTopDiLepPartonttbarPt"        );
     _h30_diffXSecTopDiLepPartonttbarY          = bookHisto1D("h30_diffXSecTopDiLepPartonttbarY"         );
     _h31_diffXSecTopDiLepPartonttbarMass       = bookHisto1D("h31_diffXSecTopDiLepPartonttbarMass"      );
+
   };
 
   void analyze(const Event& event) {
@@ -60,7 +61,7 @@ public:
       //const Particle lCand2 = ttbar.wDecays2()[0];
       _h14_diffXSecTopSemiLepPartontopPt->fill(t1P4.pT(), weight);
       _h14_diffXSecTopSemiLepPartontopPt->fill(t2P4.pT(), weight);
-      _h15_diffXSecTopSemiLepPartontopPtTtbarSys->fill(0);//t1P4AtCM.pT(), weight);
+      _h15_diffXSecTopSemiLepPartontopPtTtbarSys->fill(0, weight);//t1P4AtCM.pT(), weight);
       _h16_diffXSecTopSemiLepPartontopY->fill(t1P4.rapidity(), weight);
       _h16_diffXSecTopSemiLepPartontopY->fill(t2P4.rapidity(), weight);
       _h17_diffXSecTopSemiLepPartonttbarDelPhi->fill(dPhi, weight);
@@ -73,7 +74,7 @@ public:
     else if ( ttbar.mode() == PseudoTop::CH_FULLLEPTON ) {
       _h23_diffXSecTopDiLepPartontopPt->fill(t1P4.pT(), weight);
       _h23_diffXSecTopDiLepPartontopPt->fill(t2P4.pT(), weight);
-      _h24_diffXSecTopDiLepPartontopPtTtbarSys->fill(0);//t1P4AtCM.pT(), weight);
+      _h24_diffXSecTopDiLepPartontopPtTtbarSys->fill(0, weight);//t1P4AtCM.pT(), weight);
       _h25_diffXSecTopDiLepPartontopY->fill(t1P4.rapidity(), weight);
       _h25_diffXSecTopDiLepPartontopY->fill(t2P4.rapidity(), weight);
       _h26_diffXSecTopDiLepPartonttbarDelPhi->fill(dPhi, weight);
@@ -128,6 +129,31 @@ public:
   };
 
   void finalize() {
+    // Correction functions for TOP-12-028 paper, (parton bin height)/(pseudotop bin height)
+    const double ch14[] = {1.161660, 1.136480, 1.020996, 0.895649, 0.772136, 0.685911, 0.559711, 0.566430};
+    const double ch16[] = {2.101211, 1.099831, 0.937698, 0.883005, 0.868135, 0.882153, 0.878180, 0.941096, 1.095958, 2.056497};
+    const double ch20[] = {1.602612, 0.913407, 0.816876, 0.849766, 0.889415, 0.857082};
+    const double ch21[] = {2.461665, 1.147150, 0.908031, 0.848166, 0.814687, 0.803214, 0.824948, 0.947269, 1.122359, 2.428979};
+    const double ch22[] = {1.498358, 1.362128, 1.024490, 0.819021, 0.646227, 0.475925, 0.372441};
+
+    const double ch23[] = {0.933825, 1.069645, 1.051336, 0.919932, 0.774565};
+    const double ch25[] = {1.682022, 1.002849, 0.925246, 0.924734, 0.880097, 0.901330, 1.042041, 1.733911};
+    const double ch29[] = {1.129278, 0.908123, 0.933110, 0.963850};
+    const double ch30[] = {2.401265, 1.140515, 0.937143, 0.889803, 0.833903, 0.946386, 1.179555, 2.445021};
+    const double ch31[] = {0.803342, 1.136017, 1.206834, 1.037619, 1.081579, 0.741247};
+
+    applyCorrection(_h14_diffXSecTopSemiLepPartontopPt        , ch14);
+    applyCorrection(_h16_diffXSecTopSemiLepPartontopY         , ch16);
+    applyCorrection(_h20_diffXSecTopSemiLepPartonttbarPt      , ch20);
+    applyCorrection(_h21_diffXSecTopSemiLepPartonttbarY       , ch21);
+    applyCorrection(_h22_diffXSecTopSemiLepPartonttbarMass    , ch22);
+
+    applyCorrection(_h23_diffXSecTopDiLepPartontopPt        , ch23);
+    applyCorrection(_h25_diffXSecTopDiLepPartontopY         , ch25);
+    applyCorrection(_h29_diffXSecTopDiLepPartonttbarPt      , ch29);
+    applyCorrection(_h30_diffXSecTopDiLepPartonttbarY       , ch30);
+    applyCorrection(_h31_diffXSecTopDiLepPartonttbarMass    , ch31);
+
     normalize(_h14_diffXSecTopSemiLepPartontopPt        );
     normalize(_h15_diffXSecTopSemiLepPartontopPtTtbarSys);
     normalize(_h16_diffXSecTopSemiLepPartontopY         );
@@ -147,6 +173,16 @@ public:
     normalize(_h29_diffXSecTopDiLepPartonttbarPt      );
     normalize(_h30_diffXSecTopDiLepPartonttbarY       );
     normalize(_h31_diffXSecTopDiLepPartonttbarMass    );
+
+  };
+
+  void applyCorrection(Histo1DPtr h, const double* cf) {
+    std::vector<YODA::HistoBin1D>& bins = h->bins();
+    for ( int i=0, n=bins.size(); i<n; ++i ) {
+      const double s = cf[i];
+      YODA::HistoBin1D& bin = bins[i];
+      bin.scaleW(s);
+    }
   };
 
 private:
