8a6194a41429e21711cb63021a9b7539f440d4ef
Junghwan John Goh <jhgoh@cern.ch>
2021-02-14 22:54:47 +0900

    new RPCDigis validation module

--- Validation/MuonRPCDigis/src/SealModule.cc
+++ Validation/MuonRPCDigis/src/SealModule.cc
@@ -2,5 +2,7 @@
 #include "FWCore/PluginManager/interface/ModuleDef.h"
 
 #include "Validation/MuonRPCDigis/interface/RPCDigiValid.h"
+#include "Validation/MuonRPCDigis/interface/RPCSimDigiValid.h"
 
 DEFINE_FWK_MODULE(RPCDigiValid);
+DEFINE_FWK_MODULE(RPCSimDigiValid);
